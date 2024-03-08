e9ade8c60db45ba61792b57e03909e77147b4f98
Ereli123 <ereliruizvargas@gmail.com>
2024-03-07 21:56:58 -0400

    Rotacion de numeros

--- Clase 2/Clase 2/Program.cs
+++ Clase 2/Clase 2/Program.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace Clase_2
 {
-    internal class Program
+    public class Program
     {
+        
         static void Main(string[] args)
         {
+            /*
             int var1 = Mayor(2, 6,8);
 
             Console.WriteLine(var1);
+            */
+
+            Rotardosnumeroalprincipio();
+
+            Console.ReadLine();
+
         }
 
+        /*
         static int Mayor(int num1, int num2, int num3) 
         {
             if (num1 > num2)
@@ -27,6 +36,44 @@ namespace Clase_2
                     return num2;
                 } else { return num3;}   
             }
+        }
+        */
+
+        /*
+        static void Main()
+        {
+            Console.WriteLine("Por favor, ingrese la primera nota: ");
+            int nota1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Por favor, ingrese la segunda nota: ");
+            int nota2 = int.Parse(Console.ReadLine());
+
+            int notafinal = ((nota1 + nota2) / 2);
+
+            EvaluarNota(notafinal);
+        }
+
+        static void EvaluarNota(int nota)
+        {
+            if (nota > 50)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("Reprobado");
+            }
+        }
+        */
+
+        static void Rotardosnumeroalprincipio()
+        {
+            string numero = Console.ReadLine();
+            char unoprimeros = numero[0];
+            char dosprimeros = numero[1];
+            string resto = numero.Substring(2, numero.Length -2);
+            string resultado = resto + unoprimeros + dosprimeros;
+            Console.WriteLine(resultado);
         }
     }
 }
