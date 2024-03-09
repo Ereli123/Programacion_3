fb6ffab661edf15e97b046ff9c2fb1d26ddb1d16
Ereli123 <ereliruizvargas@gmail.com>
2024-03-08 20:15:30 -0400

    Vectores

---
+++ clase4/Vectores/Vectores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Vector de números
+            int[] numeros = new int[] { 1, 2, 3, 4, 5, };
+
+            //Vector de letras
+            char[] letras = new char[] { 'a', 'b', 'c', 'd', 'e' };
+
+            //Vectores de palabras
+            string[] palabras = 
+        }
+    }
+}
